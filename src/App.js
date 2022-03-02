28d737abca5a07c31f46514e597fe6d2464133a5
Fpsska <fpsska1337@gmail.com>
2022-03-02 21:38:57 +0300

    feat: create Footer component & add styles for stick footer at the bottom of page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 import "./assets/scss/style.scss";
 import "./App.css";
@@ -8,6 +9,8 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <main className="main"></main>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+import "./footer.scss"
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="container">
+                <div className="footer__wrapper">
+
+                    <div className="footer__content">
+                        <ul className="list">
+                            <li className="list__item">
+                                <h3 className="list__title">ГК Новоселье</h3>
+                            </li>
+                            <li className="list__item">
+                                <a className="list__link" href="#">ЖК Дует</a>
+                            </li>
+                            <li className="list__item">
+                                <a className="list__link" href="#">ЖК Эпсилон</a>
+                            </li>
+                            <li className="list__item">
+                                <a className="list__link" href="#">ЖК ЕТА</a>
+                            </li>
+                            <li className="list__item">
+                                <a className="list__link" href="#">ЖК Уютный</a>
+                            </li>
+                        </ul>
+
+                        <ul className="list">
+                            <li className="list__item">
+                                <h3 className="list__title">Партнеры</h3>
+                            </li>
+                            <li className="list__item">
+                                <a className="list__link" href="#">ЖК Эгоист</a>
+                            </li>
+                            <li className="list__item">
+                                <a className="list__link" href="#">ЖК Выборгский</a>
+                            </li>
+                            <li className="list__item">
+                                <a className="list__link" href="#">ЖК Партнера</a>
+                            </li>
+                        </ul>
+
+                        <ul className="list">
+                            <li className="list__item">
+                                <a className="list__link list__title" href="#">О компании</a>
+                            </li>
+                            <li className="list__item">
+                                <a className="list__link list__title" href="#">Пресс-релизы</a>
+                            </li>
+                            <li className="list__item">
+                                <a className="list__link list__title" href="#">Контакты</a>
+                            </li>
+                            <li className="list__item">
+                                <a className="list__link list__title" href="#">Тендеры</a>
+                            </li>
+                            <li className="list__item">
+                                <a className="list__link list__title" href="#">Коммерческие помещения</a>
+                            </li>
+                        </ul>
+
+                        <ul className="list">
+                            <li className="list__item">
+                                <a className="list__link" href="#">vkontakte</a>
+                            </li>
+                            <li className="list__item">
+                                <a className="list__link" href="#">facebook</a>
+                            </li>
+                            <li className="list__item">
+                                <a className="list__link" href="#">instagram</a>
+                            </li>
+                            <li className="list__item">
+                                <a className="list__link" href="#">youtube</a>
+                            </li>
+                        </ul>
+
+                        <form className="footer__form form" action="#">
+                            <h3 className="form__title">Подпишитесь на обновления по проектам</h3>
+                            <div className="form__group">
+                                <input className="form__input" type="text" id="subscription" placeholder="Email" />
+                                <button className="form__button">Подписаться</button>
+                            </div>
+                            <label className="form__label" htmlFor="subscription">Нажимая кнопку «Подписаться» вы даете свое согласие на обработку персональных данных</label>
+                        </form>
+                    </div>
+
+
+
+                    <div className="footer__copyright copyright">
+                        <span className="copyright__text">© n-gk.ru, 2022</span>
+                        <span className="copyright__text">Разработка сайта – cake.ru</span>
+                    </div>
+
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;

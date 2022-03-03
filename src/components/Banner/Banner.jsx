fdf610f80760a614d872738683f012e28829c399
Fpsska <fpsska1337@gmail.com>
2022-03-03 18:16:18 +0300

    feat: create Banner component & create page HTML-structure for MainPage component & refactor filter__group HTML-structure of Filter component

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import logo from "../../assets/images/logo-icon.svg"
+import "./banner.scss"
+
+const Banner = () => {
+    return (
+        <div className="banner">
+            <div className="banner__wrapper">
+                <div className="banner__content">
+                    <img className="banner__image" src={logo} alt="logo" />
+                    <div className="banner__information">
+                        <div className="banner__column">
+                            <span className="banner__count">4</span>
+                            <span className="banner__title">проекта</span>
+                        </div>
+                        <div className="banner__column">
+                            <span className="banner__count">678</span>
+                            <span className="banner__title">предложений</span>
+                        </div>
+                        <div className="banner__column">
+                            <span className="banner__count">3,26</span>
+                            <span className="banner__title">от млн. ₽</span>
+                        </div>
+                    </div>
+                    <button className="page__button banner__button">Перейти к проектам</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Banner;
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -4,40 +4,56 @@ import "./filter.scss"
 
 const Filter = () => {
     return (
-        <form action="#" className="filter">
+        <form className="filter" action="#">
             <div className="filter__wrapper">
                 <div className="filter__group filter__group--button">
-                    <div className="filter__name">Студия</div>
-                    <button className="filter__button active">1</button>
+                    <button className="filter__button active">Студия</button>
+                    <button className="filter__button">1</button>
                     <button className="filter__button">2</button>
                     <button className="filter__button">3+</button>
                 </div>
                 <div className="filter__group">
-                    <input className="filter__input" type="text" placeholder="Цена от 1 450 000" />
-                    <input className="filter__input" type="text" placeholder="до 20 000 000" />
-                    <input className="filter__input" type="range" />
+                    <input className="filter__input" type="text" placeholder="Цена от 1 450 000" value="" />
+                    <input className="filter__input" type="text" placeholder="до 20 000 000" value="" />
+                    {/*  */}
+                    <div className="price">
+                        <div className="price__progress"></div>
+                        <div className="price__range"></div>
+                        <div className="price__thumb price__thumb--left"></div>
+                        <div className="price__thumb price__thumb--rigth"></div>
+                    </div>
+
+                    <div className="price-range">
+                        <input className="price-range__input price-range__input--min" type="range" min="0" max="10000" />
+                        <input className="price-range__input price-range__input--max" type="range" min="0" max="100000" />
+                    </div>
+                    {/*  */}
                 </div>
                 <div className="filter__group">
-                    <label htmlFor="" className="filter__label">
-                        <input type="checkbox" className="filter__input filter__input--checkbox" />
+                    <label className="filter__label">
+                        <input type="checkbox" className="filter__input filter__input--checkbox" name="quarter" disabled />
+                        <span className="filter__checkbox"></span>
                         1 квартал 2022
                     </label>
-                    <label htmlFor="" className="filter__label">
-                        <input type="checkbox" className="filter__input filter__input--checkbox" />
+                    <label className="filter__label">
+                        <input type="checkbox" className="filter__input filter__input--checkbox" name="quarter" />
+                        <span className="filter__checkbox"></span>
                         3 квартал 2022
                     </label>
-                    <label htmlFor="" className="filter__label">
-                        <input type="checkbox" className="filter__input filter__input--checkbox" />
+                    <label className="filter__label">
+                        <input type="checkbox" className="filter__input filter__input--checkbox" name="quarter" />
+                        <span className="filter__checkbox"></span>
                         2 квартал 2022
                     </label>
-                    <label htmlFor="" className="filter__label">
-                        <input type="checkbox" className="filter__input filter__input--checkbox" />
+                    <label className="filter__label">
+                        <input type="checkbox" className="filter__input filter__input--checkbox" name="quarter" />
+                        <span className="filter__checkbox"></span>
                         До конца года
                     </label>
                 </div>
                 <div className="filter__group">
                     <input className="filter__input filter__input--area" type="text" placeholder="Район метро" />
-                    <SvgTemplate id="search"/>
+                    <SvgTemplate id="search" />
                 </div>
             </div>
         </form>

1caf3f4cff3cc3ed513a0e7cd37c2511ab92c0c8
Fpsska <fpsska1337@gmail.com>
2022-03-21 21:43:29 +0300

    refactor: refactor: migrate to TypeScript for mainSlice.js & create mainSliceTypes.ts, actionPayloadTypes

---
+++ src/models/mainSliceTypes.ts
@@ -0,0 +1,34 @@
+export interface checkboxInputsTypes {
+    id: string;
+    labelText: string;
+    isSelected: boolean;
+}
+
+export interface selectTemplateTypes {
+    id: number;
+    plateName: string;
+    housingNumber: string;
+    quartalNumber: string;
+    value: number;
+}
+
+export interface buttonsTypes {
+    id: number;
+    text: string;
+    isButtonSelected: boolean;
+}
+
+export interface cardsTypes {
+    id: string,
+    equipment: string,
+    suggestions: string,
+    image: string,
+    complexName: string,
+    subwayName: string,
+    walkTime: string,
+    wayMoving: string,
+    isActive: boolean,
+}
+
+
+

48b46797212c044e4debc878f827567bbd4afe93
Fpsska <fpsska1337@gmail.com>
2022-07-04 22:13:15 +0300

    refactor: now use enableCors app method with origin, methods, allowedHeaders fields in main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: true,
+    methods: 'GET',
+    allowedHeaders: 'Content-Type, Accept',
+  });
+
   await app.listen(3000);
 }
 bootstrap();

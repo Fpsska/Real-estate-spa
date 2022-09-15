2a299bf0c78152ffd0d27b6404c5b92dde777258
Fpsska <fpsska1337@gmail.com>
2022-09-15 12:46:01 +0300

    fix: wrong baseUrl, endpoint of cardTemplatesAPI func

--- src/app/api/card-templatesAPI.ts
+++ src/app/api/card-templatesAPI.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const cardTemplatesAPI = createApi({
     reducerPath: 'cardTemplatesAPI',
-    baseQuery: fetchBaseQuery({ baseUrl: 'https://cake-labs-backend.vercel.app/' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://cake-labs-backend.vercel.app/api' }),
     endpoints: (build) => ({
         getCardTemplates: build.query({
             query: () => ({
